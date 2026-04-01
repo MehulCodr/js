66d2ad9137811e6dbc6607cc8cb1e7a7cc08db2e
MehulCodr <mehulgupta0910@gmail.com>
2026-04-01 21:31:33 +0530

    clean

--- 01_datatypes.js
+++
@@ -1,6 +0,0 @@
-let name = "kunal"; // string
-let age = 19; // integer
-let islogin = false; // boolean
-console.log(typeof name); // string will print;
-console.log(typeof null); // object
-console.log(typeof undefined); // undefined
--- 03_dates.js
+++
@@ -1,6 +0,0 @@
-let mydate = new Date();
-console.log(mydate.toString());
-console.log(mydate.toDateString());
-
-let mycreated_date = new Date("2026-06-25")
-console.log(mycreated_date.toDateString());
